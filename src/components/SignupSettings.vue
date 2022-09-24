aa2427321650c550fa35f2888c914d0856e3f393
Oskildo <podolopezospina@gmail.com>
2022-09-24 17:28:29 -0500

    licencias signeup

--- src/components/SignupSettings.vue
+++ src/components/SignupSettings.vue
@@ -1,48 +1,62 @@
 <template>
     <div class="bg-white border border-iris-grey-border rounded-md mt-2">
-        <div class="grid grid-cols-8">
+        <div class="grid grid-cols-8 mt-5">
             <div class="col-span-2">
-                <h1>Add collab space member</h1>
-                <p>
+                <h1 class="text-iris-blue text-xl not-italic mt-1 mx-3 font-semibold">Sign up settings</h1>
+                <p class="text-iris-blue opacity-20 mx-3">
                     Lorem ipsum dolor sit amet consectetur, ad
                 </p>
             </div>
             <div class="col-span-6">
-                <div class="grid grid-cols-1">
-                    <div class="flex p-3 text-base font-bold text-iris-blue bg-iris-blue bg-opacity-10 rounded-lg hover:bg-opacity-20 group hover:shadow space-x-3">
-                        <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture">
-                        <div class="flex-auto space-y-0.5 font-medium text-left text-xl">
-                            <div>TLC</div>
-                        </div>
-                        <button class="bg-iris-blue hover:bg-blue-700 font-bold py-2 px-4 rounded-full">
-                            <span class="text-iris-green">
-                                Save changes
-                            </span>                    
-                        </button>
+                <div class="grid grid-cols-1 gap-4 mt-1 mx-4 my-5">
+                    <div class="flex items-center p-2 text-base font-bold text-iris-blue bg-iris-blue bg-opacity-10 rounded-lg hover:bg-opacity-20 group hover:shadow space-x-3">
+                      
+                            <div class="flex-auto space-y-0.5 font-medium text-left text-xl mx-3">
+                                <div>Closed sign-up</div>
+                                <p  class="text-iris-blue opacity-20 mx-0">Admin creates all users</p>
+                            </div>
+                        
+
+                            <div class="flex items-center pt-4 pr-2 mb-4">
+
+                                <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                
+                            </div> 
+                        
                     </div>  
 
-                    <div class="flex items-center p-3 text-base font-bold text-iris-blue bg-iris-blue bg-opacity-10 rounded-lg hover:bg-opacity-20 group hover:shadow space-x-3">
-                        <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture">
-                        <div class="flex-auto space-y-0.5 font-medium text-left text-xl">
-                            <div>TLC</div>
-                        </div>
-                        <button class="bg-iris-blue hover:bg-blue-700 font-bold py-2 px-4 rounded-full">
-                            <span class="text-iris-green">
-                                Save changes
-                            </span>
-                        </button>
+                    <div class="flex items-center p-2  text-base font-bold text-iris-blue bg-iris-blue bg-opacity-10 rounded-lg hover:bg-opacity-20 group hover:shadow space-x-3">
+                        <div class="flex-auto space-y-0.5 font-medium text-left text-xl mx-3">
+                                <div>Self-enrollment</div>
+                                <p  class="text-iris-blue opacity-20 mx-0">No approvement</p>
+                            </div>
+                        
+
+                            <div class="flex items-center pt-4 pr-2 mb-4">
+
+                                <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                
+                           
+                            </div> 
+
+
+                        
                     </div>
 
-                    <div class="flex items-center p-3 text-base font-bold text-iris-blue bg-iris-blue bg-opacity-10 rounded-lg hover:bg-opacity-20 group hover:shadow space-x-3">
-                        <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture">
-                        <div class="flex-auto space-y-0.5 font-medium text-left text-xl">
-                            <div>TLC</div>
-                        </div>
-                        <button class="bg-iris-blue hover:bg-blue-700 font-bold py-2 px-4 rounded-full">
-                            <span class="text-iris-green">
-                                Save changes
-                            </span>                    
-                        </button>
+                    <div class="flex items-center p-2 text-base font-bold text-iris-blue bg-iris-blue bg-opacity-10 rounded-lg hover:bg-opacity-20 group hover:shadow space-x-3">
+                        <div class="flex-auto space-y-0.5 font-medium text-left text-xl mx-3">
+                                <div>Self-enrollment</div>
+                                <p  class="text-iris-blue opacity-20 mx-0">White document check process</p>
+                            </div>
+                        
+
+                            <div class="flex items-center pt-4 pr-2 mb-4">
+
+                                <input id="default-radio-1" type="radio" value="" name="default-radio"
+                                class="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2">
+                                
+
+                            </div>  
                     </div>
                 </div>
             </div>
